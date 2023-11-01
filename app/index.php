7b7d82637a5286cfa033494942ac16537f5b01bc
CarlosArias <carlos.ariasalmanzar@gmail.com>
2023-10-31 20:51:18 -0400

    First commit

---
+++ app/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Practica de Docker Compose</title>
+</head>
+<body>
+    <h1 >Practica de Docker Compose</h1>
+    <h2>Carlos Arias 2022-0021</h2>
+</body>
+</html>
